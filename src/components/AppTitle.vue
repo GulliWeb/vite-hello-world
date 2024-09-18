014f6983cd64ad2c8adab32b4925cac27d8b49e5
Andrea Gulli <andreagulli11@gmail.com>
2024-09-19 00:54:59 +0200

    ex COMPLETE

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+    <h1 class="text-align-center fw-bold">LA MIA PRIMA APP IN VUE! :)</h1>
+</template>
+
+<style lang="scss" scoped>
+h1{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+
+</style>
